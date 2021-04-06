8af001b873ca92152b8a01ae9f485d7c2d5b6653
GIREESH @ DG <62746472+immrdg@users.noreply.github.com>
2021-04-06 11:09:26 +0530

    updated trail2

--- check_and_radio__buttons/mainwindow.cpp
+++ check_and_radio__buttons/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <string>
+#include <QDebug>
+#include <QFont>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -9,6 +11,7 @@ MainWindow::MainWindow(QWidget *parent)
     ui->radioButton->setCheckable(true);
     ui->radioButton_2->setCheckable(true);
     ui->radioButton_3->setCheckable(true);
+    //QTextStream ConsOut(stdout);
 }
 
 MainWindow::~MainWindow()
@@ -23,11 +26,13 @@ void MainWindow::on_checkBox_stateChanged(int arg1)
     if(arg1)
     {
 
+        qDebug()<<"Italic Is Checked";
         f.setItalic(true);
         ui->label->setFont(f);
     }
     else
        {
+        qDebug()<<"Italic Is Unchecked";
         f.setItalic(false);
         ui->label->setFont(f);
     }
@@ -38,12 +43,13 @@ void MainWindow::on_checkBox_2_stateChanged(int arg1)
     QFont f=ui->label->font();
     if(arg1)
     {
-
+        qDebug()<<"Bold Is Checked";
         f.setBold(true);
         ui->label->setFont(f);
     }
     else
        {
+        qDebug()<<"Bold Is Unchecked";
         f.setBold(false);
         ui->label->setFont(f);
     }
@@ -52,23 +58,26 @@ void MainWindow::on_checkBox_2_stateChanged(int arg1)
 void MainWindow::on_radioButton_clicked(bool checked)
 {
     if(checked)
+    {
+       qDebug()<<"Red Color Is Checked";
        ui->label->setStyleSheet("color:red");
-    else
-        ui->label->setStyleSheet("color:black");
+    }
 }
 
 void MainWindow::on_radioButton_2_clicked(bool checked)
 {
     if(checked)
+    {
+       qDebug()<<"Blue Color Is Checked";
        ui->label->setStyleSheet("color:blue");
-    else
-       ui->label->setStyleSheet("color:black");
+    }
 }
 
 void MainWindow::on_radioButton_3_clicked(bool checked)
 {
     if(checked)
+    {
+       qDebug()<<"Green Color Is Checked";
        ui->label->setStyleSheet("color:green");
-    else
-       ui->label->setStyleSheet("color:black");
+    }
 }
